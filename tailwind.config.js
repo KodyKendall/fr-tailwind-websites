651e2ad0105164918d6ad3ff9047eba436dfd373
Kody <kodyckendall@gmail.com>
2024-07-18 12:08:04 -0700

    work with tailwind offline, and added in uplifting garage door

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+          'custom-blue': '#2596be',
+        },
+      },
+    },
+    plugins: [],
+  }
